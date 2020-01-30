0e7b241e2f226f533e1322d20680820b9cdf00cf
pushenko <38810197+pushenko@users.noreply.github.com>
2020-01-31 00:07:47 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,108 +1,24 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[10]:
-
-
-from sklearn import datasets
-from sklearn import svm
-from sklearn.model_selection import GridSearchCV 
 import pandas as pd
-import numpy as np1
-#from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import train_test_split
-from joblibspark import register_spark
-from sklearn.utils import parallel_backend
-from sklearn.ensemble import RandomForestClassifier
-from pyspark.sql import SparkSession
+import pyspark
+from pyspark import SparkContext
 
+sc=SparkContext("spark://ip-172-31-47-76.eu-west-1.compute.internal:7077", "Test")
 
-# In[ ]:
+data_filepath = '/home/ubuntu/server/wine.csv'
 
+data = pd.read_csv(data_filepath)
+# data.info()
 
+n_records = data.quality.count()
+n_unique_qualities = data.quality.unique()
 
+records_str = f'Number of records: {n_records}'
+qualities_str = f'Number of unique qualities: {n_unique_qualities}'
 
+print(records_str)
+print(qualities_str)
 
-# In[2]:
-
-
-#from pyspark.ml import Pipeline
-from pyspark.ml.classification import GBTClassifier
-#from pyspark.ml.classification import RandomForestClassifier
-#from pyspark.ml.feature import IndexToString, StringIndexer, VectorIndexer
-#from pyspark.ml.evaluation import MulticlassClassificationEvaluator
-
-
-# In[3]:
-
-
-register_spark()
-
-
-# In[4]:
-
-
-wine_data = pd.read_csv('wine.csv')
-parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10]}
-svr = svm.SVC(gamma='auto')
-
-
-# In[5]:
-
-
-wine_data.head()
-
-
-# In[6]:
-
-
-# separate target from training features
-y = wine_data.quality
-X = wine_data.drop('quality', axis=1)
-
-
-# In[7]:
-
-
-# split data into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y, 
-    test_size=0.2, 
-    random_state=123, 
-    stratify=y
-)
-
-
-# In[8]:
-
-
-hyperparameters = {'randomforestclassifier__max_features': [None, 'auto', 'sqrt', 'log2'],
-                   'randomforestclassifier__max_depth': [None, 3, 5, 10],}
-
-
-# In[ ]:
-
-
-
-
-
-# In[12]:
-
-
-# fit and tune model
-clf = RandomForestClassifier()
-
-
-# In[13]:
-
-
-with parallel_backend('spark', n_jobs=3):
-    clf.fit(X_train, y_train)
-
-
-# In[ ]:
-
-
-
-
+with open('statistics.txt', 'w') as file:
+    for string in (records_str, qualities_str):
+        file.write(string + '\n')
+sc.stop()
